bbb2834c41929cf9de617f3edab86dd6baf4a0d0
ali <adadashipour@gmail.com>
2023-05-23 12:02:19 +0100

    Instead of making two separete ojects, added an object constructor and added two players in that way.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const playerOne = {
-    number: 1, 
-    marker: "X"
+function Player(name, number, marker){
+    this.name = name
+    this.number = number
+    this.marker = marker
 }
 
-const playerTwo = {
-    number: 2,
-    marker: "O"
-}
+const playerOne =  new Player(prompt("Enter the first player's name"), 1, "X")
+const playerTwo =  new Player(prompt("Enter the second player's name"), 2, "O")
 
