cd61d6f68b3dde2c87cd0f2b882abd8f339bbb3c
ali <adadashipour@gmail.com>
2023-05-29 15:48:05 +0100

    console.log is more efficient now

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ function Player(name, number, marker){
     this.name = name
     this.number = number
     this.marker = marker
+    this.sayName = function() {
+        console.log(name)
+    }
 }
 
-const playerOne =  new Player(prompt("Enter the first player's name"), 1, "X")
-const playerTwo =  new Player(prompt("Enter the second player's name"), 2, "O")
+const playerOne =  new Player(prompt("Enter the first player's name"), 1, "X");
+const playerTwo =  new Player(prompt("Enter the second player's name"), 2, "O");
 
+playerOne.sayName()
+playerTwo.sayName()
