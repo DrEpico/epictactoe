d74a8e57ab6b864a08ffc49ac643aa2a361c46f6
ali <adadashipour@gmail.com>
2023-06-12 15:10:21 +0100

    Added the game board, some styling and a module that stores each element by ID (for now).

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ const playerTwo =  new Player(prompt("Enter the second player's name"), 2, "O");
 
 playerOne.sayName()
 playerTwo.sayName()
+
+const gameBoard = (() => {
+    const a1 = document.getElementById("a1");
+    const a2 = document.getElementById("a2");
+    const a3 = document.getElementById("a3");
+    const b1 = document.getElementById("b1");
+    const b2 = document.getElementById("b2");
+    const b3 = document.getElementById("b3");
+    const c1 = document.getElementById("c1");
+    const c2 = document.getElementById("c2");
+    const c3 = document.getElementById("c3");
+})();
