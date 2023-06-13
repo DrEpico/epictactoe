b824bdda5610b67653b5a93c70473f1619da1050
ali <adadashipour@gmail.com>
2023-06-13 14:52:32 +0100

    Tried to have minimum global lines but it went kind of weird. (After all the name input prompts show up before pressing the start button)

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function Player(name, number, marker){
-    this.name = name
-    this.number = number
-    this.marker = marker
-    this.sayName = function() {
-        console.log(name)
-    }
-}
-
-const playerOne =  new Player(prompt("Enter the first player's name"), 1, "X");
-const playerTwo =  new Player(prompt("Enter the second player's name"), 2, "O");
-
-playerOne.sayName()
-playerTwo.sayName()
-
 const gameBoard = (() => {
     const cells = document.querySelectorAll("#board-container div");
 
@@ -22,7 +7,33 @@ const gameBoard = (() => {
     // Add event listeners to each cell
     cellArray.forEach(cell => {
         cell.addEventListener("click", () => {
-        // Handle the cell click event here
+            cell.innerHTML = "X";
         });
     });
 })();
+
+const gameStart = (() => {
+    function Player(name, number, marker){
+        this.name = name
+        this.number = number
+        this.marker = marker
+        this.sayName = function() {
+            console.log(name)
+        }
+    }  
+
+    const playerOne =  new Player(prompt("Enter the first player's name"), 1, "X");
+    const playerTwo =  new Player(prompt("Enter the second player's name"), 2, "O");  
+    
+    playerOne.sayName();
+    playerTwo.sayName();
+
+    return {
+        playerOne,
+        playerTwo
+    }
+})
+
+
+const gameData = gameStart(); 
+console.log(gameData.playerOne,gameData.playerTwo); 
