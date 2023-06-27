84bbcf25804eb3421a232ae5865deba32a9af1c7
ali <adadashipour@gmail.com>
2023-06-27 17:00:29 +0100

    Gave up and copied from youtube video (https://www.youtube.com/watch?v=kVE4xX-OkJo&ab_channel=DorsCodingSchool). It went good until I implemented restart function. Will do the rest tomorrow.

--- script.js
+++ script.js
@@ -1,65 +1,180 @@
-const gameBoard = (() => {
-    const cells = document.querySelectorAll("#board-container div");
+// const gameBoard = (() => {
+//     const cells = document.querySelectorAll("#board-container div");
 
-    // Convert the NodeList to an array for easier manipulation
-     const cellArray = Array.from(cells);
+//     // Convert the NodeList to an array for easier manipulation
+//      const cellArray = Array.from(cells);
   
-    // Add event listeners to each cell
-    cellArray.forEach(cell => {
-        cell.addEventListener("click", () => {
-            cell.innerHTML = "X";
-        });
-    });
+//     // Add event listeners to each cell
+//     cellArray.forEach(cell => {
+//         cell.addEventListener("click", () => {
+//             cell.innerHTML = "X";
+//         });
+//     });
+// })();
+
+// let test = true;
+ 
+// const findWinner = () => {
+//     const rows = [
+//         document.querySelectorAll(".R1"),
+//         document.querySelectorAll(".R2"),
+//         document.querySelectorAll(".R3")
+//     ];
+
+//     console.log("test 1")
+
+//     rows.forEach(row => {
+//         Array.from(row).forEach(cell => {
+//             if (cell.textContent === "X") {
+//                 console.log("X has occupied a row")
+//             }
+//             if (test === true) {
+//                 console.log("test 2")
+//             }
+//         });
+//     });
+// };
+
+// function Player(name, number, marker) {
+//     this.name = name;
+//     this.number = number;
+//     this.marker = marker;
+//     this.sayName = function() {
+//         console.log(name);
+//     };
+// }
+
+// let playerOne;
+// let playerTwo;
+
+// function startGame() {
+//     playerOne = new Player(prompt("Enter the first player's name"), 1, "X");
+//     playerTwo = new Player(prompt("Enter the second player's name"), 2, "O");
+
+//     playerOne.sayName();
+//     playerTwo.sayName();
+// }
+
+// const cells = document.querySelectorAll("#board-container div");
+// const cellArray = Array.from(cells);
+
+// cellArray.forEach(cell => {
+//     cell.addEventListener("click", () => {
+//         cell.innerHTML = "X";
+//     });
+// });
+
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", ""," ", "", ""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
+        })
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+        const square = document.querySelectorAll(".square");
+        square.forEach((square) => {
+            square.addEventListener("click", Game.handleClick);
+        })
+    }
+
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
+
+    const getGameBoard = () => gameboard; 
+
+    return {
+        render,
+        update,
+        getGameBoard
+    }
 })();
 
-let test = true;
-
-const findWinner = () => {
-    const rows = [
-        document.querySelectorAll(".R1"),
-        document.querySelectorAll(".R2"),
-        document.querySelectorAll(".R3")
-    ];
-
-    console.log("test 1")
-
-    rows.forEach(row => {
-        Array.from(row).forEach(cell => {
-            if (cell.textContent === "X") {
-                console.log("X has occupied a row")
-            }
-            if (test === true) {
-                console.log("test 2")
-            }
-        });
-    });
-};
-
-function Player(name, number, marker) {
-    this.name = name;
-    this.number = number;
-    this.marker = marker;
-    this.sayName = function() {
-        console.log(name);
-    };
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
 }
 
-let playerOne;
-let playerTwo;
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
 
-function startGame() {
-    playerOne = new Player(prompt("Enter the first player's name"), 1, "X");
-    playerTwo = new Player(prompt("Enter the second player's name"), 2, "O");
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"), 
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+        const square = document.querySelectorAll(".square");
+        square.forEach((square) => {
+            square.addEventListener("click", Game.handleClick);
+        })
+    }
 
-    playerOne.sayName();
-    playerTwo.sayName();
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+        if (Gameboard.getGameBoard()[index] !== "")
+            return;
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+
+        if(checkForWin(Gameboard.getGameBoard(),players[currentPlayerIndex].mark)){
+            gameOver = true;
+            alert(`${players[currentPlayerIndex].name} won!`)
+        }
+
+
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++){
+            Gameboard.update(i, "");
+        }
+        Gameboard.render();
+    }
+
+    return {
+        start,
+        restart,
+        handleClick
+    }
+})();
+
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2]
+        [3, 4, 5]
+        [6, 7, 8]
+        [0, 3, 6]
+        [1, 4, 7]
+        [2, 5, 8]
+        [0, 4, 8]
+        [2, 4, 6]
+    ]
+    for (let i = 0; i < winningCombinations.length; i++){
+        const [a, b, c] = winningCombinations[i];
+        if (board[a] && board[a] === board[b] && board [a] === board[c]){
+            return true;
+        }       
+    }
+    return false;
 }
 
-const cells = document.querySelectorAll("#board-container div");
-const cellArray = Array.from(cells);
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+    Game.restart();
+})
 
-cellArray.forEach(cell => {
-    cell.addEventListener("click", () => {
-        cell.innerHTML = "X";
-    });
-});
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", ()=> {
+    Game.start();
+})
